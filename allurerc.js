c652320e039ab656a2f210c3ead9bf32ee342b46
Jessica Lai <jessica.lai@univention.de>
2026-08-14 14:17:25 +0200

    ci: fix the allure configuration
    Issue univention/dev/education/user-management#186

--- allurerc.js
+++ allurerc.js
@@ -1,19 +1,23 @@
 export default {
   historyPath: "./allure-history.jsonl",
   environments: {
-    "Python 3.7": {
+    "python-3-7": {
+      title: "Python 3.7",
       matcher: ({ labels }) =>
         labels.some(l => l.name === "python_version" && l.value === "3.7")
     },
-    "Python 3.8": {
+    "python-3-8": {
+      title: "Python 3.8",
       matcher: ({ labels }) =>
         labels.some(l => l.name === "python_version" && l.value === "3.8")
     },
-    "Python 3.9": {
+    "python-3-9": {
+      title: "Python 3.9",
       matcher: ({ labels }) =>
         labels.some(l => l.name === "python_version" && l.value === "3.9")
     },
-    "Python 3.10": {
+    "python-3-10": {
+      title: "Python 3.10",
       matcher: ({ labels }) =>
         labels.some(l => l.name === "python_version" && l.value === "3.10")
     }
